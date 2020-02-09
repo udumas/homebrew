3571fc0a3920d86d42f058ce8299b9683d630426
Joel Salisbury <udumas@users.noreply.github.com>
2020-02-08 19:54:03 -0500

    Create freecad-dev.rb

---
+++ Casks/freecad-dev.rb
@@ -0,0 +1,12 @@
+cask 'freecad-dev' do
+  version '0.19,19443'
+  sha256 '44bd499cfcec15cfd9346ad242e4f39896b37bf6800cba6d84f95b4495dc58e0'
+
+  # github.com/FreeCAD/FreeCAD was verified as official when first introduced to the cask
+  url "https://github.com/FreeCAD/FreeCAD/releases/download/#{version.before_comma}/FreeCAD_#{version.major_minor}-#{version.after_comma}-OSX-x86_64-conda-Qt5-Py3.dmg"
+  appcast 'https://github.com/FreeCAD/FreeCAD/releases.atom'
+  name 'FreeCAD'
+  homepage 'https://www.freecadweb.org/'
+
+  app 'FreeCAD.app'
+end
