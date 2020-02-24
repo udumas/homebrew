957639560cf6279ba648430c499c6dfd045e3166
Joel Salisbury <udumas@users.noreply.github.com>
2020-02-23 20:53:29 -0500

    Update freecad-dev.rb
    changed to newer version

--- Casks/freecad-dev.rb
+++ Casks/freecad-dev.rb
@@ -1,6 +1,6 @@
 cask 'freecad-dev' do
-  version '0.19,19443'
-  sha256 '44bd499cfcec15cfd9346ad242e4f39896b37bf6800cba6d84f95b4495dc58e0'
+  version '0.19,19694'
+  sha256 'deda14ce0caaaab2a7acb5e5e99625cfc6f67d546e81e4e967efcf2d42d11ec4'
 
   # github.com/FreeCAD/FreeCAD was verified as official when first introduced to the cask
   url "https://github.com/FreeCAD/FreeCAD/releases/download/#{version.before_comma}_pre/FreeCAD_#{version.major_minor}-#{version.after_comma}-macOS-x86_64-conda-Qt5-Py3.dmg"
