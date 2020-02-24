f065c78b9dfe2fa550c883c2665a81d2a2631501
Joel Salisbury <udumas@users.noreply.github.com>
2020-02-23 21:18:13 -0500

    Update freecad-dev.rb
    updated the link to current version. Current version does not contain qt5 py3

--- Casks/freecad-dev.rb
+++ Casks/freecad-dev.rb
@@ -3,7 +3,7 @@ cask 'freecad-dev' do
   sha256 'deda14ce0caaaab2a7acb5e5e99625cfc6f67d546e81e4e967efcf2d42d11ec4'
 
   # github.com/FreeCAD/FreeCAD was verified as official when first introduced to the cask
-  url "https://github.com/FreeCAD/FreeCAD/releases/download/#{version.before_comma}_pre/FreeCAD_#{version.major_minor}-#{version.after_comma}-macOS-x86_64-conda-Qt5-Py3.dmg"
+  url "https://github.com/FreeCAD/FreeCAD/releases/download/#{version.before_comma}_pre/FreeCAD_#{version.major_minor}-#{version.after_comma}-macOS-x86_64-conda.dmg"
   appcast 'https://github.com/FreeCAD/FreeCAD/releases.atom'
   name 'FreeCAD'
   homepage 'https://www.freecadweb.org/'
