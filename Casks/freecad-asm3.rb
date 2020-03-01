07f4c3a488425fa512cd2b19b0a953ca2f800dfb
Joel Salisbury <udumas@users.noreply.github.com>
2020-02-29 21:51:49 -0500

    added sha256 hash

--- Casks/freecad-asm3.rb
+++ Casks/freecad-asm3.rb
@@ -1,5 +1,6 @@
 cask 'freecad-asm3' do
 
+  sha256 '3cc64c8570b96fdd886b1def7cc00820c2c2ef7f9a40e8b8a15e8fae2f0565db'
   # github.com/FreeCAD/FreeCAD was verified as official when first introduced to the cask
   url "https://github.com/realthunder/FreeCAD_assembly3/releases/download/0.10.2/FreeCAD-asm3-OSX-Conda-Py3-Qt5-20190703-x86_64.dmg"
   appcast 'https://github.com/FreeCAD/FreeCAD/releases.atom'
